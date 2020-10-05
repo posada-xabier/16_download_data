2825c23ba44a6e9adbc8836ee00c5bd5ac4dffa4
posada-xabier <posada.xabier.duran@gmail.com>
2020-10-05 10:54:50 +0200

    plot graph max temperature

--- sitka_highs.py
+++ sitka_highs.py
@@ -1,4 +1,6 @@
 import csv
+import matplotlib.pyplot as plt 
+
 filename = 'data/sitka_weather_07-2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
@@ -13,3 +15,17 @@ with open(filename) as f:
 		high = int(row[5])
 		highs.append(high)
 print(highs)
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(highs, c='red')
+
+plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
+
+
