b121b7d04f748027bc3dd4cf01e1e9fb15ab61b9
posada-xabier <posada.xabier.duran@gmail.com>
2020-10-05 11:18:43 +0200

    temperaturas year 2018

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,7 +2,7 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt 
 
-filename = 'data/sitka_weather_07-2018_simple.csv'
+filename = 'data/sitka_weather_2018_simple.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
@@ -19,7 +19,7 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red')
 
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high temperatures - 2018 ", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
