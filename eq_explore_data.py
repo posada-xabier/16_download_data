30ac48b47c0614ac550b3c897b3857f047af5e27
posada-xabier <posada.xabier.duran@gmail.com>
2020-10-06 09:53:29 +0200

    calculo numero de terremotos y a partir de json crear big dico , next big dico with indentation

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -1,7 +1,15 @@
 import json
+
 filename = 'data/eq_data_1_day_m1.json'
+
 with open(filename) as f:
 	all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+
+
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file, 'w') as f:
 	json.dump(all_eq_data, f, indent=4)
