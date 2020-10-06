05dd8b9135e34e17d0ffd4984791acfb78f72e4b
posada-xabier <posada.xabier.duran@gmail.com>
2020-10-06 10:14:06 +0200

    print:  lista magnitudes , lista longitudes, lista latitudes

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -7,15 +7,24 @@ with open(filename) as f:
 	all_eq_data = json.load(f)
 
 # next you get the number of terremotos
+# result >> 158
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
 
 # next extracting magnitudes for Earth Q.
-mags = []
+# result >> [0.96, 1.2, 4.3, 3.6, 2.1, 4, 1.06, 2.3, 4.9, 1.8]
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
 	mag = eq_dict['properties']['mag']
+	lon = eq_dict['geometry']['coordinates'][0]
+	lat = eq_dict['geometry']['coordinates'][1]
 	mags.append(mag)
+	lons.append(lon)
+	lats.append(lat)
+
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
 
 # next in same folder your code creates a data python file more readable because of indentation
 readable_file = 'data/readable_eq_data.json'
