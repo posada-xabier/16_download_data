f257006b1ebe0d89ad768d12e84acd8e3668ce2a
posada-xabier <posada.xabier.duran@gmail.com>
2020-10-06 11:48:07 +0200

    mapamundi improved dots with color and proportional to mag sizes

--- eq_world_map.py
+++ eq_world_map.py
@@ -1,5 +1,7 @@
 import json
 
+# We import the Scattergeo chart type and the Layout class, 
+# and then import the offline module to render the map
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
@@ -24,10 +26,32 @@ for eq_dict in all_eq_dicts:
     hover_texts.append(title)
 
 # MAP THE EARTHQUAKES.
-data = [Scattergeo(lon=lons, lat=lats)]
-my_layout = Layout(title='Global Earthquakes')
+# we define a list called data. We create the Scattergeo object inside this list.
+# modo 1     data = [Scattergeo(lon=lons, lat=lats)]  next you see alternative to mode 1
+data = [{
+	'type':'scattergeo',
+	'lon': lons,
+	'lat': lats,
+	'marker':{
+		'size': [5*mag for mag in mags],
+		'color':mags,
+		'colorscale':'Viridis',
+		'reversescale':True,
+		'colorbar':{'title':'Magnitude'},
+	}
+}]
 
+# You can plot more than one data set in any visualization, 
+# but the Scattergeo chart type allows you to overlay a scatter plot of geographic data on a map
+# the simplest just list latit + list longit.
+# We give the chart an appropriate title
+my_layout = Layout(title='Global Earthquakes')
+# and create a dictionary
+# called '"fig"' that contains the data and the layout
 fig = {'data':data, 'layout':my_layout}
+
+#Finally, we pass fig
+# to the plot() function along with a descriptive filename for the output
 offline.plot(fig, filename='global_earthquakes.html')
 
 
